335aa33660b93d97e0919db06748e8ec499191d5
tony2055 <antonino_auditore@hotmail.com>
2020-05-19 18:43:37 -0300

    Implementação do Diálogo
    Implementação funcional e inicial do sistema de diálogo

---
+++ Assets/Scripts/DialogueScripts/DialogueManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+    private AudioSource aud;
+
+    private Queue<string> sentences;
+
+    // Use this for initialization
+    void Start()
+    {
+        sentences = new Queue<string>();
+        aud = GetComponent<AudioSource>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("isOpen", true);
+        aud.enabled = true;
+        nameText.text = dialogue.name;
+
+        //Limpa as sentenças anteriores para começar a próxima
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    //Próxima Sentença
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    //Animação para cada letra da sentença
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            aud.Play();
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    //Fim do Diálogo
+    void EndDialogue()
+    {
+        aud.enabled = false;
+        animator.SetBool("isOpen", false);
+    }
+
+}
---
+++ Assets/Scripts/DialogueScripts/DialogueTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Invoke("TriggerDialogue", 0.5f);
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+
+}
